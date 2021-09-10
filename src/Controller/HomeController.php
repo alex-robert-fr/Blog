273f9f23674f28381bf094fec4e5a295d3b6bebb
Alex Robert <contact.alexrobert04@gmail.com>
2021-09-10 22:08:01 +0200

    BACK - [DATABASE]: Recover all articles

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Article;
+use App\Repository\ArticleRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -10,9 +11,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class HomeController extends AbstractController
 {
     #[Route('/')]
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository): Response
     {
-        return $this->render('home/index.html.twig');
+        $articles = $articleRepository->findAll();
+        return $this->render('home/index.html.twig', [
+            'articles' => $articles
+        ]);
     }
 
     #[Route('/insert')]
