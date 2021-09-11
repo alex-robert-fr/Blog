b6fdd771fc43da4658d011223f34861ec4d5ad20
Alex Robert <contact.alexrobert04@gmail.com>
2021-09-11 17:17:08 +0200

    FRONT - [DATABASE]: Recover comments for ArticlePage

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Commentaire;
 use App\Form\CommentaireType;
 use App\Repository\ArticleRepository;
+use App\Repository\CommentaireRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,10 +14,18 @@ use Symfony\Component\Routing\Annotation\Route;
 class ArticleController extends AbstractController
 {
     #[Route('/article/{id}')]
-    public function index(int $id, ArticleRepository $articleRepository, Request $request): Response
+    public function index(int $id, ArticleRepository $articleRepository, CommentaireRepository $commentaireRepository, Request $request): Response
     {
+        /**
+         * *---Recovery articles---*
+         */
         $article = $articleRepository->find($id);
         $id = strval($id);
+
+        /**
+         * *---Recovery comments---*
+         */
+        $commentsArticle = $commentaireRepository->findBy(['article' => $id]);
 
         /**
          * *---Form---*
@@ -37,6 +46,7 @@ class ArticleController extends AbstractController
             'id' => $id,
             'form' => $form->createView(),
             'article' => $article,
+            'comments' => $commentsArticle
         ]);
     }
 }
